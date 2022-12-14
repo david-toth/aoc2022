49ae3c2112dc5a36a17c91887702083d819d7334
David Toth <davidtoth99@gmail.com>
2022-12-14 13:39:00 -0500

    Update day14.py

--- day14.py
+++ day14.py
@@ -1,100 +1,96 @@
 with open("inputs/day14.txt") as f:
     data = [l.strip() for l in f.readlines()]
-    
-def parse_line(l):
-    c = []
-    for i in l.split(" -> "):
-        for j in i.split(","):
-            c.append(tuple(map(int, j)))
-   return c
 
-def build_lines(c1, c2):
-    p = []
+def parse_line(l):
+    coords = []
+    l = l.split(" -> ")
+    for i in l:
+        xy = i.split(",")
+        coords.append(tuple(map(int, xy)))
+    return coords
+
+def draw_line(c1, c2):
+    pts = []
     if c1[0] == c2[0]:
         if c1[1] < c2[1]:
-            a = c1[1]
-            b = c2[1] + 1
+            for i in range(c1[1], c2[1]+1):
+                pts.append((c1[0], i))
         else:
-            a = c2[1]
-            b = c1[1] + 1
-        for i in range(a, b):
-            p.append((c1[0], i))
+            for i in range(c2[1], c1[1]+1):
+                pts.append((c1[0], i))
     else:
-        if c1[0] < c2[]:
-            a = c1[0]
-            b = c2[0] + 1
+        if c1[0] < c2[0]:
+            for i in range(c1[0], c2[0]+1):
+                pts.append((i, c1[1]))
         else:
-            a = c2[0]
-            b = c1[0] + 1
-        for i in range(a, b):
-            p.append((i, c1[1]))
-    return p
+            for i in range(c2[0], c1[0]+1):
+                pts.append((i, c1[1]))
+    return pts
 
+#------- Part 1
 pts = set()
-
-# Find points 
 for l in data:
-    pl = parse_line(l)
-    if len(pl) == 2:
-        pts.update(build_lines(pl[0], pl[1]))
+    lp = parse_line(l)
+    if len(lp) == 2:
+        pts.update(draw_line(*lp))
     else:
-        for i in range(1, len(pl)):
-            pts.update(build_lines(pl[i-1], pl[i]))
+        for i in range(1, len(lp)):
+            pts.update(draw_line(*lp[i-1:i+1]))
 
 ymax = max(i[1] for i in pts)
-sand = (500, 0)
-sx, sy = sand
-num_particles = 0
-#-------------- Part 1
+s = (500, 0)
+sx, sy = s
+nsand = 0
+
 while True:
     blocked = True
-    for dx, dy in ((0, 1), (1, 1), (-1, 1)):
+    for dx, dy in [(0,1), (-1,1), (1,1)]:
         if (sx+dx, sy+dy) not in pts:
             sx += dx
             sy += dy
             blocked = False
             break
-    if sy > 2*ymax: # assume abyss is anything > double the deepest rock
-        break 
+    if sy > 2*ymax:
+        break
     if blocked:
-        num_particles += 1
+        nsand += 1
         pts.add((sx, sy))
-        sx, sy = sand
+        sx, sy = s
         
-print(num_particles)
-#-------------- Part 2
-pts = set()
+print(nsand)
 
-# Find points 
+#-------- Part 2
+pts = set()
 for l in data:
-    pl = parse_line(l)
-    if len(pl) == 2:
-        pts.update(build_lines(pl[0], pl[1]))
+    lp = parse_line(l)
+    if len(lp) == 2:
+        pts.update(draw_line(*lp))
     else:
-        for i in range(1, len(pl)):
-            pts.update(build_lines(pl[i-1], pl[i]))
+        for i in range(1, len(lp)):
+            pts.update(draw_line(*lp[i-1:i+1]))
 
 ymax = max(i[1] for i in pts)
-sand = (500, 0)
-sx, sy = sand
-num_particles = 0
 
-for x in (-1000, 1000):
+for x in range(-1000, 1000):
     pts.add((x, ymax+2))
-    
+
+s = (500, 0)
+sx, sy = s
+nsand = 0
+
 while True:
     blocked = True
-    for dx, dy in ((0, 1), (1, 1), (-1, 1)):
+    for dx, dy in [(0,1), (-1,1), (1,1)]:
         if (sx+dx, sy+dy) not in pts:
             sx += dx
             sy += dy
             blocked = False
             break
     if blocked:
-        num_particles += 1
-        if (sx, sy) == sand:
+        nsand += 1
+        if (sx, sy) == s:
             break
         pts.add((sx, sy))
-        sx, sy = sand
+        sx, sy = s
         
-print(num_particles)
+print(nsand)
